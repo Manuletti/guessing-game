f2f8da9e598eb6efa63a9f462bbf7e2c61351466
Igor Tarbeev <actimel@yandex.ru>
2022-07-06 02:18:53 +0300

    rewriting config

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< Updated upstream
   plugins: [vue()],
   base: '/guessing-game/',
+=======
+  base: '/guessing-game/',
+  plugins: [vue()],
+>>>>>>> Stashed changes
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
