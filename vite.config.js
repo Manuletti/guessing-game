e490dd4db98df006e2c7661d4f7076df5d8d68a1
Igor Tarbeev <actimel@yandex.ru>
2022-07-06 16:17:05 +0300

    for gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/guessing-game/',
+  base: '/guessing-game/dist/',
   plugins: [vue()],
   resolve: {
     alias: {
