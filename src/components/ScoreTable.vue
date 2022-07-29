a249ad6a23843a2636e18470ed8ab09e43148eaa
Igor Tarbeev <actimel@yandex.ru>
2022-07-29 19:32:24 +0300

    Score style and logic

--- src/components/ScoreTable.vue
+++ src/components/ScoreTable.vue
@@ -9,8 +9,27 @@ defineProps({
 </script>
 
 <template>
-    <h1>
-       Score table
-       {{score}}
-    </h1>
+    <h1>Scores!</h1>
+    <table>
+        <thead>
+            <tr>
+                <td>Nickname</td>
+                <td>Points</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(value, key) in score">
+                <td>{{key}}</td>
+                <td>{{value}}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
+
+<style scoped>
+thead {
+ color:  rgb(39, 50, 52);
+ background-color: lime;
+ font-size: 1.5em;
+}
+</style>
--- src/views/ScoreView.vue
+++ src/views/ScoreView.vue
@@ -1,29 +1,21 @@
 <script setup>
 import ScoreTableVue from '../components/ScoreTable.vue';
-import { ref, onMounted, nextTick } from 'vue';
+import { ref } from 'vue';
 
 
 var score = ref({'before': 'fetch'})
-async function getData () {
-    fetch('http://localhost:3001/')
-        .then((response) => {
-            return response.json();
+fetch('http://localhost:3001/')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        score.value = data
         })
-        .then((data) => {
-            score.value = data
-            console.group('Results:')
-            console.log('the data was wrritten: ', data);
-            console.log('The variable: ', score)
-        });
-}
-
-getData()
-console.log('Sync log of score: ', score.value)
 
 </script>
 
 <template>
     <div>
-        <ScoreTableVue :score='score' />
+        <ScoreTableVue :score="score" />
     </div>
 </template>
